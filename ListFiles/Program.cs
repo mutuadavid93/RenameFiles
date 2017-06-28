a431fadbac6f5a75d553eae727aab8736ac07ee6
David Mutua <mutuadavi@gmail.com>
2017-06-28 15:27:51 +0300

    Looped through Files in a Folder and Renamed them

---
+++ ListFiles/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ListFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string targetDir = @"G:\LYNDATUTS\Typescript Essential Training";
+            string cloneDir = targetDir + @"G:\LYNDATUTS\Typescript Essentials";
+            listFilesInDir(targetDir, cloneDir);
+
+            // Keep the Console Window open
+            Console.WriteLine("Press Enter to Close");
+            Console.Read();
+        }
+
+        public static void listFilesInDir(string WorkDir, string newDir)
+        {
+            var files = new DirectoryInfo(WorkDir).GetFiles("*.mp4", SearchOption.TopDirectoryOnly);
+
+            foreach (FileInfo file in files)
+            {
+                var x = file.Name;
+                string realName = x.Split('-')[0];
+
+                var fullPath = WorkDir + @"\" +x;
+                string suffix = new String(x.ToCharArray().Where(c => Char.IsDigit(c)).ToArray());
+                // suffix is fine
+                File.Move(fullPath, newDir + @"\" + suffix+"-"+ realName + ".mp4");
+            }
+
+            Console.WriteLine("Mission ^_^ Complete");
+        }
+    }
+}
